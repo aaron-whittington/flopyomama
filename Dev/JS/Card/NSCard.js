0aa4570e303f8e6ff43782af43b6c098f90d9b95
Aaron Whittington <aaron.whittington@magari-internet.de>
2017-08-09 14:52:02 +0200

    standardizeSuits function

--- Dev/JS/Card/NSCard.js
+++ Dev/JS/Card/NSCard.js
@@ -5,11 +5,27 @@ nsCard =  {
             return card2.get('rank') - card1.get('rank');
         else 
             return card2.get('suit') - card1.get('suit');
-
     }, 
     sortCardArray: function(cardArray, preserveOriginal = true) {
         var cardClone = preserveOriginal ? cardArray.slice() : cardArray;  
         cardClone.sort(nsCard.compareCards);
+        return cardClone;
+    }, 
+    standardizeSuits: function(cardArray, preserveOriginal = true) {
+        var cardClone = preserveOriginal ? cardArray.slice() : cardArray;  
+        var suitsFound = [];
+        var targetSuits = [4,3,2,1];
+        var i;
+        for( i=0; i<cardClone.length; i++) {
+            if(suitsFound.indexOf(cardClone[i].get('suit')) < 0) {
+                suitsFound.push(cardClone[i].get('suit'));
+            } 
+        }
+        var indexOfFoundSuit;
+        for( i=0; i<cardClone.length; i++) {
+            indexOfFoundSuit = suitsFound.indexOf(cardClone[i].get('suit'));
+            cardClone[i].set('suit', targetSuits[indexOfFoundSuit]);
+        }
         return cardClone;
     }
 };
--- spec/Card/nsCardSpec.js
+++ spec/Card/nsCardSpec.js
@@ -37,7 +37,7 @@ describe("NSCard", function() {
             expect(result[2].get('suit')).toEqual(2); 
 
         });
-        xit('should modify original if preserveoriginal is false, otherwise not', function() {
+        it('should modify original if preserveoriginal is false, otherwise not', function() {
             input = (new CardList('5D6HAS')).models; 
             result = nsCard.sortCardArray(input);
             expect(input[0].get('rank')).toEqual(5);
@@ -45,6 +45,30 @@ describe("NSCard", function() {
 
             nsCard.sortCardArray(input, false);
             expect(input[0].get('rank')).toEqual(14);
+        });
+    });
+
+    describe('standardizeSuits', function() {
+        var input, result;
+        it('should standardize suits correctly, spades first', function() {
+            input = (new CardList('5D7C4S')).models; 
+            result = nsCard.standardizeSuits(input);
+ 
+            expect(result.length).toEqual(3); 
+            expect(result[0].toString()).toEqual('5S'); 
+            expect(result[1].toString()).toEqual('7H'); 
+            expect(result[2].toString()).toEqual('4D'); 
+
+            input = (new CardList('ACKCQCJCTC')).models; 
+            result = nsCard.standardizeSuits(input);
+ 
+            expect(result.length).toEqual(5); 
+            expect(result[0].toString()).toEqual('AS'); 
+            expect(result[1].toString()).toEqual('KS'); 
+            expect(result[2].toString()).toEqual('QS'); 
+            expect(result[3].toString()).toEqual('JS'); 
+            expect(result[4].toString()).toEqual('TS'); 
+
         });
     });
 });
