90f580764c45935821ed596bed11cdb46113f96d
aaron-whittington <aaronwhittington@Aarons-MBP.fritz.box>
2017-08-28 18:33:24 +0200

    list actions

---
+++ Dev/Vue/Round/Street.vue
@@ -0,0 +1,17 @@
+<template>
+ <li> 
+    {{street.label}}
+    <ol type='A' v-if='street.actions.length > 0'>
+       <li v-for='action in street.actions'>
+          {{action.who}} {{action.verb}} {{action.amount}}
+      </li>
+     </ol>  
+  </li>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default Vue.component('round-street', {
+        props: ['street']
+    });
+</script>
